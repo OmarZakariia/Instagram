c18ca7bf73b6c5d12d622cb05a210173e4fffe8b
OmarZakariia <Omar.zakaria88@hotmail.com>
2021-03-30 18:24:37 +0200

    Done with part 2

---
+++ Instagram/Resources/AuthManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthManager.swift
+//  Instagram
+//
+//  Created by Omar Zakaria on /253/21.
+//
+
+import Foundation
---
+++ Instagram/Resources/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  Instagram
+//
+//  Created by Omar Zakaria on /253/21.
+//
+
+import Foundation
